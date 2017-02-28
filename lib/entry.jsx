a5d4dbbc5bcee9aa364aedfed9e1da09c28d2ae0
Ken Tran <loschorts@gmail.com>
2017-02-28 13:38:38 -0800

    add entry boilerplate

---
+++ lib/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", ()=> {
+	render(
+		<h1>React skeleton</h1>,
+		document.querySelector("#root")
+	);
+});
